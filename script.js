5438d0b1ca2634ad4d4b5a5d7231ee34c5c16e5f
Adrian Ferraz <ferrazadrian102@gmail.com>
2026-05-15 21:08:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var ul = document.getElementById('ul')
+
+function btn(){
+    if(ul.style.display === 'flex'){
+        ul.style.display = 'none'
+    }else{
+        ul.style.display = 'flex'
+    }
+}
